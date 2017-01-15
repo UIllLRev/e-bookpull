b0b579c222d0333468ede328b396bcdd0cdc892c
Matt Loar <matt@loar.name>
2017-01-14 22:37:43 -0600

    Add foreign key

--- api/schema.sql
+++ api/schema.sql
@@ -1,27 +1,6 @@
 -- --------------------------------------------------------
 
 -- 
--- Table structure for table `sources`
--- 
-
-CREATE TABLE `sources` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `author_code` int(6) NOT NULL,
-  `type` enum('B','C','J','L','M','P') COLLATE utf8_bin DEFAULT NULL,
-  `citation` text COLLATE utf8_bin,
-  `url` text COLLATE utf8_bin,
-  `comments` text COLLATE utf8_bin,
-  `ordered` date DEFAULT NULL,
-  `status_code` enum('N','E','M','R','X','XP','XR') COLLATE utf8_bin NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`id`),
-  KEY `type` (`type`),
-  KEY `author_code` (`author_code`),
-  KEY `status_code` (`status_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
--- 
 -- Table structure for table `works`
 -- 
 
@@ -35,4 +14,25 @@ CREATE TABLE `works` (
   `bookpuller` text COLLATE utf8_bin,
   `duedate` date DEFAULT NULL,
   PRIMARY KEY (`author_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `sources`
+-- 
+
+CREATE TABLE `sources` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `author_code` int(6) NOT NULL FOREIGN KEY `author_code` REFERENCES `works` (`author_code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `type` enum('B','C','J','L','M','P') COLLATE utf8_bin DEFAULT NULL,
+  `citation` text COLLATE utf8_bin,
+  `url` text COLLATE utf8_bin,
+  `comments` text COLLATE utf8_bin,
+  `ordered` date DEFAULT NULL,
+  `status_code` enum('N','E','M','R','X','XP','XR') COLLATE utf8_bin NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`),
+  KEY `author_code` (`author_code`),
+  KEY `status_code` (`status_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
