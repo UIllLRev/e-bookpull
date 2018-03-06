608afe873533f22888fe789095668d83b64eac43
Matt Loar <matt@loar.name>
2018-03-05 21:18:24 -0600

    Enable deploy to dev

--- config/deploy.js
+++ config/deploy.js
@@ -8,6 +8,19 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
+      ENV['sentry'] = {
+          publicUrl: 'https://dev.illinoislawreview.org/members/bookpull',
+          sentryUrl: 'https://sentry.io',
+          sentryOrganizationSlug: 'illinois-law-review',
+          sentryProjectSlug: 'frontend',
+          sentryApiKey: 'unusedbutneverthelessrequired',
+          sentryBearerApiKey: '0d38300cdb494c0e80bf2a09ce2f3b838a06d13e6a2640b9a605bf66752408c8'
+      };
+      ENV['scp'] = {
+          username: 'ilr_dev',
+          host: 'chain-lightning.dreamhost.com',
+          path: 'dev.illinoislawreview.org/members/bookpull'
+      };
     // configure other plugins for development deploy target here
   }
 
