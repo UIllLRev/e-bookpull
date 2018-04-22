c35013d81952831be15b31b1245893941468d94a
Matt Loar <matt@loar.name>
2018-04-22 13:52:29 -0500

    Disable API pagination

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,20 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-    namespace: 'members/bookpull/api/2'
+    namespace: 'members/bookpull/api/2',
+    buildQuery(snapshot) {
+        let query = {};
+        if (snapshot) {
+            let { include } = snapshot;
+
+            if (include) {
+                query.include = include;
+            }
+        }
+        query.page = { size: 0 };
+        return query;
+    },
+    findHasMany(store, snapshot, url, relationship) {
+        return this._super(store, snapshot, url + "?" + encodeURIComponent("page[size]") + "=0", relationship);
+    }
 });
