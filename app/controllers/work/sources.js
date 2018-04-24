857a06fea0379ad828ee65e2359f438eec97223b
Matt Loar <matt@loar.name>
2018-04-23 21:57:49 -0500

    Fix loading substate

--- app/controllers/work/sources.js
+++ app/controllers/work/sources.js
@@ -2,10 +2,10 @@ import Controller from '@ember/controller';
 import { filterBy } from '@ember/object/computed';
 
 export default Controller.extend({
-    books: filterBy('model.sources', 'type', 'B'),
-    cases: filterBy('model.sources', 'type', 'C'),
-    journals: filterBy('model.sources', 'type', 'J'),
-    legislative: filterBy('model.sources', 'type', 'L'),
-    miscellaneous: filterBy('model.sources', 'type', 'M'),
-    periodicals: filterBy('model.sources', 'type', 'P')
+    books: filterBy('model', 'type', 'B'),
+    cases: filterBy('model', 'type', 'C'),
+    journals: filterBy('model', 'type', 'J'),
+    legislative: filterBy('model', 'type', 'L'),
+    miscellaneous: filterBy('model', 'type', 'M'),
+    periodicals: filterBy('model', 'type', 'P')
 });
--- app/routes/work/sources.js
+++ app/routes/work/sources.js
@@ -1,5 +1,9 @@
 import EmberRoute from '@ember/routing/route';
 
 export default EmberRoute.extend({
-    breadCrumb: null
+    breadCrumb: null,
+    model() {
+        let work = this.modelFor('work');
+        return work.get('sources');
+    }
 });
