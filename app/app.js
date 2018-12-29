7933490f4d93f3587382be8a18ecf45664a39cd4
Matt Loar <matt@loar.name>
2018-12-28 20:48:34 -0600

    Upgrade to new Sentry client

--- app/app.js
+++ app/app.js
@@ -2,13 +2,24 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import * as Sentry from '@sentry/browser';
+
+let release = document.querySelector("meta[name='sentry:revision']").attributes.content;
+
+Sentry.init({
+    dsn: 'https://14dda427f59c4ec68fbe4a4a93998506@sentry.io/110958',
+    integrations: [new Sentry.Integrations.Ember()],
+    release: release !== undefined ? release.value : '',
+    environment: config.environment
+});
 
 let App;
 
 App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+  Sentry
 });
 
 loadInitializers(App, config.modulePrefix);
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,34 +3,38 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-   sassOptions: {
-      includePaths: ['bower_components/material-design-lite/src']
-    },
-    sourcemaps: {
-      enabled: true,
-      extensions: ['js']
-    }
-  });
+    var app = new EmberApp(defaults, {
+        sassOptions: {
+            includePaths: ['bower_components/material-design-lite/src']
+        },
+        sourcemaps: {
+            enabled: true,
+            extensions: ['js']
+        }
+    });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+    // Use `app.import` to add additional libraries to the generated
+    // output files.
+    //
+    // If you need to use different assets in different
+    // environments, specify an object as the first parameter. That
+    // object's keys should be the environment name and the values
+    // should be the asset to use in that environment.
+    //
+    // If the library that you are including contains AMD or ES6
+    // modules that you would like to import into your application
+    // please specify an object with the list of modules as keys
+    // along with the exports of each module as its value.
 
-  app.import(app.bowerDirectory + '/material-design-lite/material.min.js');
-  app.import(app.bowerDirectory + '/dialog-polyfill/dialog-polyfill.css');
-  app.import(app.bowerDirectory + '/dialog-polyfill/dialog-polyfill.js');
-  app.import('node_modules/raven-js/dist/raven.js');
-  app.import('node_modules/raven-js/dist/plugins/ember.js');
+    app.import(app.bowerDirectory + '/material-design-lite/material.min.js');
+    app.import(app.bowerDirectory + '/dialog-polyfill/dialog-polyfill.css');
+    app.import(app.bowerDirectory + '/dialog-polyfill/dialog-polyfill.js');
 
-  return app.toTree();
+    app.import('node_modules/@sentry/browser/dist/index.js', {
+        using: [
+            { transformation: 'cjs', as: '@sentry/browser' }
+        ]
+    });
+
+    return app.toTree();
 };
